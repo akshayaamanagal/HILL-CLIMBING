e5bcc3678953981cd7ac226eeef5b3f258d8ec74
Akshayaa <119389066+akshayaamanagal@users.noreply.github.com>
2024-09-28 08:53:29 +0530

    Update hill climbing.py

--- hill climbing.py
+++ hill climbing.py
@@ -7,7 +7,7 @@
 import random
 import string
 def generate_random_solution(answer):
-    l=TYPE THE CODE HERE ,FIND THE LEN OF answer and store in l
+    l=len(answer)
     return [random.choice(string.printable) for _ in range(l)]
 def evaluate(solution,answer):
     print(solution)
@@ -20,7 +20,7 @@ def evaluate(solution,answer):
         #ord(s) - ord(t) calculates the difference between the ASCII values of the characters s and t.
          #abs() takes the absolute value of this difference to ensure that it is non-negative. This is important because the difference could be negative if s is less than t in terms of ASCII value.
          #The absolute value ensures that the difference is always positive or zero.
-        diff +=Type the code here by referring to the above description 
+        diff += abs(ord(s) - ord(t))    return diff
     return diff
 def mutate_solution(solution):
     ind=random.randint(0,len(solution)-1)
